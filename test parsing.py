25f32a78c03db1bd3764252f572d5adb70df6d17
goshaobolonskiy <133564434+goshaobolonskiy@users.noreply.github.com>
2024-01-02 17:04:34 +0300

    ебал рот

--- test parsing.py
+++ test parsing.py
@@ -1,27 +1,35 @@
 from bs4 import BeautifulSoup
-import pandas as pd
 import requests
+import time
+import csv
+import json
 import os
 
 all_name = []
+start = 0
+count = 50
+for i in range(9):
+    url = f"https://steamcommunity.com/market/search/render/?query=&start={start}&count={count}&search_descriptions=0&sort_column=popular&sort_dir=desc&appid=730"
+    headers = {
+        "Accept": "*/*",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/23.1.5.708 Yowser/2.5 Safari/537.36"}
+    html = requests.get(url = url, headers = headers)
 
-url = "https://steamcommunity.com/market/search/render/?query=&start=0&count=500&search_descriptions=0&sort_column=popular&sort_dir=desc&appid=730"
-headers = {
-    "Accept": "*/*",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/23.1.5.708 Yowser/2.5 Safari/537.36"}
-html = requests.get(url = url, headers = headers)
+    # with open('html.html', 'w') as file:
+    #     file.write(html.text)
 
-# with open('html.html', 'w') as file:
-#     file.write(html.json().get('results_html'))
-
-soup = BeautifulSoup(html.json().get('results_html'), "lxml")
-    # Получение кусков кода html
-name = soup.find_all("span", class_ = "market_listing_item_name")
-   # Получение цены и имени
-for k in name:
-    all_name.append(k.text)
-# print(all_name)
-print(all_name)
-
-df = pd.DataFrame(all_name)
-gfg_csv_data = df.to_csv('names.csv', index = False)
+    soup = BeautifulSoup(html.json().get("results_html"), "lxml")
+    # print(soup)
+    name = soup.find_all("span", class_ = "market_listing_item_name")
+    # Получение цены и имени
+    for k in name:
+        all_name.append(k.text)
+    start += 50
+    count += 50
+with open('test.txt', 'w') as file:
+    for i in all_name:
+        file.write(f'{i}\n')
+# csv.register_dialect('my_di', lineterminator="\r", delimiter=0, quotechar=' ')
+# with open("information.csv", "a", encoding = "utf-8") as file:
+#     writer = csv.writer(file, 'my_di')
+#     writer.writerow(all_name[1])
